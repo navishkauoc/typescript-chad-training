2b000793d75a13187302a2185e8ef9c288cf8d27
Navishka Kularathna <navishka.randika@gmail.com>
2020-04-27 14:03:15 +0530

    Use of if conditionals

--- 03-loops-and-arrays/sports.js
+++ 03-loops-and-arrays/sports.js
@@ -1,9 +1,14 @@
 var sports = ["Golf", "Cricket", "Tennis", "Swimming"];
-for (var i = 0; i < sports.length; i++) {
-    console.log(sports[i]);
-}
+// for(let i=0; i<sports.length; i++){
+//     console.log(sports[i]);
+// }
 //Use of simplified for loops
 for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
     var tempSport = sports_1[_i];
-    console.log(tempSport);
+    if (tempSport == "Cricket") {
+        console.log(tempSport + " is my favorite");
+    }
+    else {
+        console.log(tempSport);
+    }
 }
--- 03-loops-and-arrays/sports.ts
+++ 03-loops-and-arrays/sports.ts
@@ -1,10 +1,15 @@
 let sports: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
 
-for(let i=0; i<sports.length; i++){
-    console.log(sports[i]);
-}
+// for(let i=0; i<sports.length; i++){
+//     console.log(sports[i]);
+// }
 
 //Use of simplified for loops
 for(let tempSport of sports){
-    console.log(tempSport);
+    if(tempSport == "Cricket"){
+        console.log(tempSport + " is my favorite");
+    } else{
+        console.log(tempSport);
+    }
+
 }
