48664a9aaa25d030771856e076cdf0186118e800
Navishka Kularathna <navishka.randika@gmail.com>
2020-04-27 13:36:25 +0530

    Use of simplified for loops

--- 03-loops-and-arrays/sports.js
+++ 03-loops-and-arrays/sports.js
@@ -2,3 +2,8 @@ var sports = ["Golf", "Cricket", "Tennis", "Swimming"];
 for (var i = 0; i < sports.length; i++) {
     console.log(sports[i]);
 }
+//Use of simplified for loops
+for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
+    var tempSport = sports_1[_i];
+    console.log(tempSport);
+}
--- 03-loops-and-arrays/sports.ts
+++ 03-loops-and-arrays/sports.ts
@@ -3,3 +3,8 @@ let sports: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
 for(let i=0; i<sports.length; i++){
     console.log(sports[i]);
 }
+
+//Use of simplified for loops
+for(let tempSport of sports){
+    console.log(tempSport);
+}
