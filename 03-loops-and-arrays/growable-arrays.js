cc73ba1615ce149f15f7c7dbabd06cf133063562
Navishka Kularathna <navishka.randika@gmail.com>
2020-04-27 14:16:16 +0530

    Growable arrays - use of push() method

---
+++ 03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,7 @@
+var sports01 = ["Golf", "Cricket", "Tennis"];
+sports01.push("Baseball");
+sports01.push("Football");
+for (var _i = 0, sports01_1 = sports01; _i < sports01_1.length; _i++) {
+    var tempSports = sports01_1[_i];
+    console.log(tempSports);
+}
---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sports01: string[] = ["Golf", "Cricket", "Tennis"];
+
+sports01.push("Baseball");
+sports01.push("Football");
+
+for(let tempSports of sports01){
+    console.log(tempSports);
+}
