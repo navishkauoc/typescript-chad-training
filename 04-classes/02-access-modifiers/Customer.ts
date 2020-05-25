1b2a2a99f60050594f53484ed292efef1c25f0f6
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-26 00:25:43 +0530

    TypeScript - Accessors updated

--- 04-classes/02-access-modifiers/Customer.ts
+++ 04-classes/02-access-modifiers/Customer.ts
@@ -4,11 +4,27 @@ class Customer{
 
     constructor(theFirst: string, theLast: string){
         this._firstName = theFirst;
-        this.lastName = theLast;
+        this._lastName = theLast;
+    }
+
+    public get firstName(): string{
+        return this._firstName;
+    }
+
+    public set firstName(value: string){
+        this._firstName = value;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+    
+    public set lastName(value: string) {
+        this._lastName = value;
     }
 }
 
-let myCustomer = new Customer;
+let myCustomer = new Customer("Martin", "Dixon");
 
 myCustomer.firstName = "Martin";
 myCustomer.lastName = "Dixon";
