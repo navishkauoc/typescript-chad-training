732440997387327162ea70391fb4f5c158dcad0f
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-30 15:13:21 +0530

    Circle.ts created

---
+++ 05-inheritance/Circle.ts
@@ -0,0 +1,17 @@
+import { Shape } from './Shape';
+
+export class Circle extends Shape{
+    
+    constructor(theX: number, theY: number, private _radius: number){
+        super(theX, theY);
+    }
+
+    public get radius(): number{
+        return this._radius;
+    }
+
+    getInfo(): string{
+        return super.getInfo() + ', radius= ${this._radius}';
+    }
+
+}
