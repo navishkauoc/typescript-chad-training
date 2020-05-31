ab07baaf9493fbb608e737f9bfbd129faee3e81e
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-31 14:09:36 +0530

    Main app and tsconfig.json added

---
+++ 07-interfaces/GolfCoach.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var GolfCoach = /** @class */ (function () {
+    function GolfCoach() {
+    }
+    GolfCoach.prototype.getDailyWorkout = function () {
+        return "Hit 100 balls";
+    };
+    return GolfCoach;
+}());
+exports.GolfCoach = GolfCoach;
